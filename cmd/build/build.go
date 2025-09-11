4939c1c95aea92d4a04774ff153e5004fd50c824
pberton <1789738+pberton@users.noreply.github.com>
2025-09-10 18:39:02 -0700

    fix logic

--- cmd/build/build.go
+++ cmd/build/build.go
@@ -155,12 +155,10 @@ func runBuild(cmd *cobra.Command, args []string) error {
 	}
 	// Check if spec-type was explicitly provided
 	specTypeExplicit := cmd.Flags().Changed("spec-type")
-	if !specTypeExplicit {
-		if file == PlanSpecFileName {
-			specType = ServicePlanSpecType
-		} else {
-			specType = DockerComposeSpecType
-		}
+	if file == PlanSpecFileName {
+		specType = ServicePlanSpecType
+	} else {
+		specType = DockerComposeSpecType
 	}
 
 	name, err := cmd.Flags().GetString("name")
