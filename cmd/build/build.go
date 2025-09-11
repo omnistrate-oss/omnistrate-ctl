08cd4e7deb4cef16697a0f071efd04f0e02b1136
pberton <1789738+pberton@users.noreply.github.com>
2025-09-10 18:51:33 -0700

    guess type

--- cmd/build/build.go
+++ cmd/build/build.go
@@ -274,15 +274,11 @@ func runBuild(cmd *cobra.Command, args []string) error {
 			} else {
 				// check for compose file
 				file = ComposeFileName
-				if !specTypeExplicit {
-					specType = DockerComposeSpecType
-				}
+				specType = DockerComposeSpecType
 				if _, err := os.Stat(file); os.IsNotExist(err) {
 					// If the file doesn't exist and wasn't explicitly provided, we check if there is a spec file
 					file = PlanSpecFileName
-					if !specTypeExplicit {
-						specType = ServicePlanSpecType
-					}
+					specType = ServicePlanSpecType
 					if _, err := os.Stat(file); os.IsNotExist(err) {
 						err = errors.New("no compose or spec file found, please provide a valid file using --file flag")
 						utils.PrintError(err)
