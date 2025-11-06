15828005ba4c0fb7ceee2a57b1d2b45ba4707742
Alok Nikhil <alok@omnistrate.com>
2025-11-06 08:54:10 -0800

    Don't restore cursor if spinner was not used

--- internal/utils/error_handler.go
+++ internal/utils/error_handler.go
@@ -3,8 +3,6 @@ package utils
 import (
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/chelnak/ysmrr"
 )
@@ -15,29 +13,15 @@ func EnsureCursorRestoration() {
 	os.Stdout.Sync()
 }
 
-// StartSpinnerWithCleanup starts a spinner and sets up cleanup handlers for all exit paths
-func StartSpinnerWithCleanup(sm ysmrr.SpinnerManager) {
-	// Set up signal handler to restore cursor on interrupt (Ctrl+C)
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-sigChan
-		sm.Stop()
-		EnsureCursorRestoration()
-		os.Exit(130) // Standard exit code for SIGINT
-	}()
-
-	sm.Start()
-}
-
 func HandleSpinnerError(spinner *ysmrr.Spinner, sm ysmrr.SpinnerManager, err error) {
 	if spinner != nil {
 		spinner.Error()
+		EnsureCursorRestoration()
 	}
 	if sm != nil {
 		sm.Stop()
+		EnsureCursorRestoration()
 	}
-	EnsureCursorRestoration()
 	PrintError(err)
 }
 
@@ -45,9 +29,10 @@ func HandleSpinnerSuccess(spinner *ysmrr.Spinner, sm ysmrr.SpinnerManager, messa
 	if spinner != nil {
 		spinner.UpdateMessage(message)
 		spinner.Complete()
+		EnsureCursorRestoration()
 	}
 	if sm != nil {
 		sm.Stop()
+		EnsureCursorRestoration()
 	}
-	EnsureCursorRestoration()
 }
