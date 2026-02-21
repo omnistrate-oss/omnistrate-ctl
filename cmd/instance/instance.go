e607f8fc4b9a2296b104759d8bcdde3ad817111a
pberton <1789738+pberton@users.noreply.github.com>
2026-02-21 00:52:34 -0800

    remove

--- cmd/instance/instance.go
+++ cmd/instance/instance.go
@@ -32,7 +32,6 @@ func init() {
 	Cmd.AddCommand(listSnapshotsCmd)
 	Cmd.AddCommand(describeSnapshotCmd)
 	Cmd.AddCommand(copySnapshotCmd)
-	Cmd.AddCommand(deleteSnapshotCmd)
 	Cmd.AddCommand(restoreCmd)
 	Cmd.AddCommand(adoptCmd)
 	Cmd.AddCommand(versionUpgradeCmd)
