fa96e50c80a841f6f718e27890c291382be7b2ad
pberton <1789738+pberton@users.noreply.github.com>
2026-03-03 13:37:35 -0800

    update

--- cmd/upgrade/upgrade.go
+++ cmd/upgrade/upgrade.go
@@ -201,7 +201,7 @@ func run(cmd *cobra.Command, args []string) error {
 
 		// Fall back to a lightweight describe if version wasn't in search results
 		if sourceVersion == "" {
-			describeRes, descErr := dataaccess.DescribeResourceInstance(cmd.Context(), token, serviceID, environmentID, instanceID, false)
+			describeRes, descErr := dataaccess.DescribeResourceInstance(cmd.Context(), token, serviceID, environmentID, instanceID)
 			if descErr != nil {
 				utils.HandleSpinnerError(spinner, sm, descErr)
 				return descErr
