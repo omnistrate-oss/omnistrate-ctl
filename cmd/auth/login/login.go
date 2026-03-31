13cbacb5516e61746facf8ad5c791ed05e1c4e3f
pberton <1789738+pberton@users.noreply.github.com>
2026-03-30 21:23:50 -0700

    remove

--- cmd/auth/login/login.go
+++ cmd/auth/login/login.go
@@ -24,10 +24,7 @@ omnistrate-ctl login --email email --password password
   cat ~/omnistrate_pass.txt | omnistrate-ctl login --email email --password-stdin
 
 # Login with email and password from stdin. Save the password in an environment variable and use echo to read it
-  echo $OMNISTRATE_PASSWORD | omnistrate-ctl login --email email --password-stdin
-
-# Login with Microsoft Entra SSO
-omnistrate-ctl login --entra`
+  echo $OMNISTRATE_PASSWORD | omnistrate-ctl login --email email --password-stdin`
 
 	loginWithEmailAndPassword loginMethod = "Login with email and password"
 	loginWithGoogle           loginMethod = "Login with Google"
